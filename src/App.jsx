3a28ea7cfd96e330b2b8813b310bb841c76dc11e
sachithrakalani <kalanisathyani@gmail.com>
2024-11-23 17:16:21 +0530

    Install Font awesome icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import HomePageHeader from "./Components/HomePageHeader"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
--- src/Components/HomePageHeader.jsx
+++ src/Components/HomePageHeader.jsx
@@ -1,11 +1,13 @@
-export default function HomePageHeader(){
-    return(
+export default function HomePageHeader() {
+  return (
     <header>
-        <div>
-            <h1>Cinema Hub</h1>
-            <input placeholder="Search for Moives..."></input>
-            <button>Search</button>
-        </div>
+      <div>
+        <h1>Cinema Hub</h1>
+        <input placeholder="Search for Moives..."></input>
+        <button>
+          <i className="fas fa-search"></i> Search
+        </button>
+      </div>
     </header>
-    )
+  );
 }
