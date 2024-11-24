1ed771e951742fa8f85a2cb82f9b91bf7e8b1f80
sachithrakalani <kalanisathyani@gmail.com>
2024-11-24 20:22:32 +0530

    Style home page header using css modules

--- src/Components/HomePageHeader.jsx
+++ src/Components/HomePageHeader.jsx
@@ -1,16 +1,18 @@
+import styles from "./homepageheader.module.css";
+
 export default function HomePageHeader() {
   return (
     <header>
-      <div>
-        <h1>Cinema Hub</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Cinema Hub</h1>
         <input placeholder="Search for Moives..."></input>
         <button>
           <i className="fas fa-search"></i> Search
         </button>
         <button>Sign In</button>
-        <span> OFFERS </span>
-        <span> | </span>
-        <span> CONTACT US</span>
+        <span>OFFERS</span>
+        <span>|</span>
+        <span>CONTACT US</span>
       </div>
     </header>
   );
