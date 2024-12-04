498745cd165497c5e4164bea29aa1c3f5b002af2
sachithrakalani <kalanisathyani@gmail.com>
2024-12-04 11:30:36 +0530

    Add Arrows in left and right

--- src/Components/HomePageCarousel.jsx
+++ src/Components/HomePageCarousel.jsx
@@ -1,9 +1,13 @@
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
 export default function HomePageCarousel({ data: carouselData }) {
   return (
     <div>
+        <BsArrowLeftCircleFill />
       {carouselData.map((items, index) => {
         return <img src={items.src} alt={items.alt} key={index}/>;
       })}
+      <BsArrowRightCircleFill />
     </div>
   );
 }
