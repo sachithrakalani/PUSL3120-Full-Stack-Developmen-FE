dc5f9d770ce7f4eb5e2da7b297914f5ad0d3e945
sachithrakalani <kalanisathyani@gmail.com>
2024-12-04 12:22:56 +0530

    Style left and right arrow icons

--- src/Components/HomePageCarousel.jsx
+++ src/Components/HomePageCarousel.jsx
@@ -4,11 +4,29 @@ import style from "./homepagecarousel.module.css";
 export default function HomePageCarousel({ data: carouselData }) {
   return (
     <div className={style.carousel}>
-        <BsArrowLeftCircleFill />
+        <BsArrowLeftCircleFill
+        style={{
+          position: "absolute",
+          filter: "drop-shadow(0px 0px 5px #555)",
+          width: "3rem",
+          height: "3rem",
+          color: "white",
+          cursor: "pointer",
+        }}
+        className={style.arrowLeft}
+      />
       {carouselData.map((items, index) => {
         return <img src={items.src} alt={items.alt} key={index} className={style.slide}/>;
       })}
-      <BsArrowRightCircleFill />
+      <BsArrowRightCircleFill style={{
+          position: "absolute",
+          filter: "drop-shadow(0px 0px 5px #555)",
+          width: "3rem",
+          height: "3rem",
+          color: "white",
+          cursor: "pointer",
+        }}
+        className={style.arrowRight} />
       <span>
         {carouselData.map((_,index)=>{
           return(
