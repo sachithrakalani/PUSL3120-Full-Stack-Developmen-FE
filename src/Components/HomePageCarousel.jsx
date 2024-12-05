aa55100aed68292553d13b427b07bea285198661
sachithrakalani <kalanisathyani@gmail.com>
2024-12-05 10:47:50 +0530

    Add background color to image carousel section

--- src/Components/HomePageCarousel.jsx
+++ src/Components/HomePageCarousel.jsx
@@ -6,54 +6,72 @@ export default function HomePageCarousel({ data: carouselData }) {
   const [slide, setSlides] = useState(0);
 
   const nextSlide = () => {
-    setSlides(slide === carouselData .length - 1 ? 0 : slide + 1);
+    setSlides(slide === carouselData.length - 1 ? 0 : slide + 1);
   };
 
   const prevSlide = () => {
-    setSlides(slide === 0 ? carouselData .length - 1 : slide - 1);
+    setSlides(slide === 0 ? carouselData.length - 1 : slide - 1);
   };
 
   return (
     <div className={style.carouselContainer}>
-      <div className={style.carousel}>
-        <BsArrowLeftCircleFill
-        style={{
-          position: "absolute",
-          filter: "drop-shadow(0px 0px 5px #555)",
-          width: "2rem",
-          height: "2rem",
-          color: "white",
-          cursor: "pointer",
-          marginTop:"200px",
-        }}
-        className={style.arrowLeft}
-        onClick={prevSlide}
-      />
-      {carouselData.map((items, index) => {
-        return <img src={items.src} alt={items.alt} key={index} className={slide === index ? `${style.slide}` : `${style.slideHidden}`}/>;
-      })}
-      <BsArrowRightCircleFill 
-        style={{
-          position: "absolute",
-          filter: "drop-shadow(0px 0px 5px #555)",
-          width: "2rem",
-          height: "2rem",
-          color: "white",
-          cursor: "pointer",
-          marginTop:"200px",
-        }}
-        className={style.arrowRight}
-        onClick={nextSlide}
-        />
-      <span className={style.indicators}>
-        {carouselData.map((_,index)=>{
-          return(
-            <button key={index} onClick={() => setSlides(index)} className = {slide === index ? `${style.indicator}` : `${style.indicatorinactive}`}></button>
-          )
-        })}
-      </span>
+      <div className={style.carouselContainer}>
+        <div className={style.carousel}>
+          <BsArrowLeftCircleFill
+            style={{
+              position: "absolute",
+              filter: "drop-shadow(0px 0px 5px #555)",
+              width: "2rem",
+              height: "2rem",
+              color: "white",
+              cursor: "pointer",
+              marginTop: "200px",
+            }}
+            className={style.arrowLeft}
+            onClick={prevSlide}
+          />
+          {carouselData.map((items, index) => {
+            return (
+              <img
+                src={items.src}
+                alt={items.alt}
+                key={index}
+                className={
+                  slide === index ? `${style.slide}` : `${style.slideHidden}`
+                }
+              />
+            );
+          })}
+          <BsArrowRightCircleFill
+            style={{
+              position: "absolute",
+              filter: "drop-shadow(0px 0px 5px #555)",
+              width: "2rem",
+              height: "2rem",
+              color: "white",
+              cursor: "pointer",
+              marginTop: "200px",
+            }}
+            className={style.arrowRight}
+            onClick={nextSlide}
+          />
+          <span className={style.indicators}>
+            {carouselData.map((_, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => setSlides(index)}
+                  className={
+                    slide === index
+                      ? `${style.indicator}`
+                      : `${style.indicatorinactive}`
+                  }
+                ></button>
+              );
+            })}
+          </span>
+        </div>
+      </div>
     </div>
-    </div>
-    
   );
 }
