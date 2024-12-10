454ddc22710a5f690bea70ed157351ea6212d487
sachithrakalani <kalanisathyani@gmail.com>
2024-12-10 18:01:41 +0530

    Style Coming soon section using css module

--- src/Components/ComingSoonSection.jsx
+++ src/Components/ComingSoonSection.jsx
@@ -1,13 +1,25 @@
-export default function ComingSoonSection({data:carouselData}){
-    return(
-        <div>
-            <h1>COMING SOON</h1>
-            {carouselData.map((movies,index)=>{
-                return (
-                    <img src={movies.src} alt={movies.alt} key={index}/>
-                )
-                
-            })}
+import style from "./comingsoonsection.module.css";
+
+export default function NowShowingPage({ data: carouselData }) {
+  return (
+    <div>
+      <div className={style.nowshowing}>
+        <h1>COMING SOON</h1>
+        <div className={style.imgecontainer}>
+          {carouselData.map((movies, index) => {
+            return (
+              <div key={index} className={style.card}>
+                <img
+                  src={movies.src}
+                  alt={movies.alt}
+                  className={style.cardImg}
+                />
+                <button className={style.button} onClick={null}>Buy Tickets</button>
+              </div>
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
