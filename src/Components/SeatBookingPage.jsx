9030f60a2d3d5b32c2770258e5ac8f73c8057d2b
sachithrakalani <kalanisathyani@gmail.com>
2024-12-22 17:43:28 +0530

    Style seat booking page

--- src/Components/SeatBookingPage.jsx
+++ src/Components/SeatBookingPage.jsx
@@ -1,11 +1,13 @@
 import HomePageHeader from "./HomePageHeader";
+import styles from "./seatbookingpage.module.css";
 
 export default function SeatBooking() {
-  const rows = ["A", "B", "C", "D", "E", "F", "G", "I", "J", "K"];
+  const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
   const seatsPerRow = 10;
+
   const renderSeats = () => {
     return rows.map((row) => (
-      <div key={row}>
+      <div key={row} className={styles.row}>
         {Array.from({ length: seatsPerRow }, (_, index) => {
           const seat = `${row}${index + 1}`;
           return <div key={seat}>{seat}</div>;
@@ -13,28 +15,34 @@ export default function SeatBooking() {
       </div>
     ));
   };
+
   return (
     <div>
       <HomePageHeader />
-      <div>SCREEN</div>
-      <div>{renderSeats()}</div>
-      <div>
-        <div>
-          <div>Available</div>
+      <div className={styles.container}>
+        <div className={styles.screen}>SCREEN</div>
+        <div className={styles.seats}>{renderSeats()}</div>
+        <div className={styles.legend}>
+          <div className={styles.legendItem}>
+            <div className={`${styles.seat} ${styles.available}`}></div>
+            <span>Available</span>
+          </div>
+          <div className={styles.legendItem}>
+            <div className={`${styles.seat} ${styles.selected}`}></div>
+            <span>Selected</span>
+          </div>
+          <div className={styles.legendItem}>
+            <div></div>
+            <span>Unavailable</span>
+          </div>
         </div>
-        <div>
-          <div>Selected</div>
+        <div className={styles.info}>
+          <div className={styles.seatInfo}>
+            <span>Seat Info:</span>
+            <div></div>
+          </div>
+          <div className={styles.total}>Total (LKR):</div>
         </div>
-        <div>
-          <div>Unavailable</div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <span>Seat Info:</span>
-          <div>None</div>
-        </div>
-        <div>Total (LKR): 0</div>
       </div>
     </div>
   );
