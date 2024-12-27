0030cce193bf9d9a0f612c67c5ba1bb1f5c10c1a
sachithrakalani <kalanisathyani@gmail.com>
2024-12-27 11:37:27 +0530

    Implement SingleMoviePage component with dynamic movie details and calendar display

--- src/Components/SingleMoviePage.jsx
+++ src/Components/SingleMoviePage.jsx
@@ -1,8 +1,26 @@
 import HomePageHeader from "./HomePageHeader";
-// import image4 from "./../../public/assets/images/4th.jpg";
 import style from "./singlemoviepage.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function SingleMoviePage() {
+  const { id } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/movie/${id}`)
+      .then((response) => {
+        console.log(response.data); // Debug response
+        setMovieDetails(response.data.movie);
+      })
+      .catch((error) => console.error("Error fetching movie details:", error));
+  }, [id]);
+
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
 
   const dates = [
     { day: "WED", date: 20 },
@@ -14,21 +32,29 @@ export default function SingleMoviePage() {
 
   return (
     <div>
-      <div>
-        <HomePageHeader />
-      </div>
-      <div>
+      <HomePageHeader />
+      <div className={style.back}>
         <div className={style.imageContainer}>
-          <img src={image4} alt={image4.alt} className={style.img}/>
-        </div>
-      </div>
-      <div className={style.calendarContainer}>
-        {dates.map((item) => (
-          <div key={item.date}>
-            <div className={style.day}>{item.day}</div>
-            <div className={style.date}>{item.date}</div>
+          {console.log("MOVIE DETAILS", movieDetails)}
+          <img
+            src={movieDetails.nowshowingImage}
+            alt={movieDetails.title || "Movie Poster"}
+            className={style.img}
+          />
+          <div className={style.details}>
+            <h3>{movieDetails.title}</h3>
+            <h3>{movieDetails.description}</h3>
+            <h3>{movieDetails.releaseDate}</h3>
           </div>
-        ))}
+        </div>
+        <div className={style.calendarContainer}>
+          {dates.map((item) => (
+            <div key={item.date}>
+              <div className={style.day}>{item.day}</div>
+              <div className={style.date}>{item.date}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
