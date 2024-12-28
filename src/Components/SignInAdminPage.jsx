8bcac68aff1099cfe8e99396f4ca1dda49650996
sachithrakalani <kalanisathyani@gmail.com>
2024-12-28 08:43:13 +0530

    Retrive data to database in admin sign in page

--- src/Components/SignInAdminPage.jsx
+++ src/Components/SignInAdminPage.jsx
@@ -3,6 +3,28 @@ import HomePageHeader from "./HomePageHeader";
 import style from "./signinpage.module.css";
 
 export default function SignInAdminPage() {
+  const handleLogin = async () => {
+    const email = document.querySelector("[name='Email']").value;
+    const password = document.querySelector("[name='Password']").value;
+
+    const response = await fetch("http://localhost:3000/admin/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("role", data.role);
+      localStorage.setItem("id", data.id);
+      alert("Login successful!");
+      window.location.href = role === "user" ? "/homepage" : "/admindashboard";
+    } else {
+      alert(data.message);
+    }
+  };
+
   return (
     <div className={style.signcontainer}>
       <HomePageHeader />
@@ -19,11 +41,14 @@ export default function SignInAdminPage() {
                 color: "white",
               }}
             >
-              Sign In
+              <button type="button" onClick={() => handleLogin()}>
+                Sign In
+              </button>
             </Link>
           </button>
           <div>
-            Not a member?<Link to="/signupadminanduser">Register Now</Link>
+            Not a member?
+            <Link to="/signupadminanduser">Register Now</Link>
           </div>
         </div>
       </center>
